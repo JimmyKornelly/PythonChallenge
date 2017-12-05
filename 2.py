f91a2537941ba50d6ebd198dd923245c49a56468
JimmyKornelly <34228705+JimmyKornelly@users.noreply.github.com>
2017-12-05 19:54:49 +0700

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,15 @@
+﻿import os
+path = 'C:\\Users\\Konst\\Desktop'
+os.chdir(path)
+with open('text.txt','r',encoding='utf8') as file:
+    text = file.readline().split()  
+res = []
+for word in text:
+    if len(word) > 6:
+        res.append(word.capitalize())
+    else:
+         res.append(word + ' ')
+with open('text1.txt','w',encoding='utf8') as file_out:
+    file_out.write(str(res))      
+print (res)
+input ()
